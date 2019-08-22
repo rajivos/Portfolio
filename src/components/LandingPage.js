9dca733c6aede2869cf104e7572cb8fc783afed7
Rajiv Khushiram <rajivkhushiram@hotmail.com>
2019-08-22 16:26:07 +1000

    transition with body

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -53,7 +53,7 @@ class HomepageHeading extends Component {
 
   render() {
     const { mobile } = this.props;
-    const {  visible } = this.state;
+    const { visible } = this.state;
 
     return (
       <Container text>
@@ -92,7 +92,14 @@ class HomepageHeading extends Component {
         </Transition>
         <center>
           <Transition nimation={"fade"} duration={2700} visible={visible}>
-            <div><Button primary  size="big" >Resume</Button> <Button positive size="big" >Projects</Button></div>
+            <div>
+              <Button primary size="big">
+                Resume
+              </Button>{" "}
+              <Button positive size="big">
+                Projects
+              </Button>
+            </div>
           </Transition>
         </center>
       </Container>
@@ -161,18 +168,22 @@ class DesktopContainer extends Component {
             >
               <Menu fixed={fixed ? "top" : null} inverted={true} size="large">
                 <Container>
-                <Transition
+                  <Transition
                     visible={calculations.topPassed}
                     animation="fade down"
                     duration={1000}
-                  ><Menu.Item as="a" active>
-                    Home
-                  </Menu.Item></Transition>
+                  >
+                    <Menu.Item as="a" active>
+                      Home
+                    </Menu.Item>
+                  </Transition>
                   <Transition
                     visible={calculations.topPassed}
                     animation="fade down"
                     duration={1500}
-                  ><Menu.Item as="a">Work</Menu.Item></Transition>
+                  >
+                    <Menu.Item as="a">Work</Menu.Item>
+                  </Transition>
                   <Transition
                     visible={calculations.topPassed}
                     animation="scale"
@@ -293,7 +304,23 @@ ResponsiveContainer.propTypes = {
 };
 
 class HomepageLayout extends Component {
-  state = {};
+  state = {
+    calculations: {
+      direction: "none",
+      height: 0,
+      width: 0,
+      topPassed: false,
+      bottomPassed: false,
+      pixelsPassed: 0,
+      percentagePassed: 0,
+      topVisible: false,
+      bottomVisible: false,
+      fits: false,
+      passing: false,
+      onScreen: false,
+      offScreen: false
+    }
+  };
 
   contextRef = createRef();
   handleUpdate = (e, { calculations }) => {
@@ -301,151 +328,111 @@ class HomepageLayout extends Component {
   };
 
   render() {
+    const { calculations } = this.state;
+
     return (
       <ResponsiveContainer>
-        <Ref innerRef={this.contextRef}>
-          <Visibility onUpdate={this.handleUpdate} updateOn="repaint">
-            <Segment style={{ padding: "8em 0em" }} vertical>
-              <Grid container stackable verticalAlign="middle">
-                <Grid.Row>
-                  <Grid.Column width={8}>
+        <Visibility onUpdate={this.handleUpdate} updateOn="repaint">
+          <Segment style={{ padding: "8em 0em" }} vertical>
+            <Grid container stackable verticalAlign="middle">
+              <Grid.Row>
+                <Grid.Column width={8}>
+                  <Transition
+                    visible={calculations.onScreen}
+                    animation="fade up"
+                    duration={1000}
+                  >
                     <Header as="h3" style={{ fontSize: "2em" }}>
                       We Help Companies and Companions
                     </Header>
+                  </Transition>
+                  <Transition
+                    visible={calculations.onScreen}
+                    animation="fade up"
+                    duration={1200}
+                  >
                     <p style={{ fontSize: "1.33em" }}>
                       We can give your company superpowers to do things that
                       they never thought possible. Let us delight your customers
                       and empower your needs... through pure data analytics.
                     </p>
-                    <Header as="h3" style={{ fontSize: "2em" }}>
-                      We Make Bananas That Can Dance
-                    </Header>
-                    <p style={{ fontSize: "1.33em" }}>
-                      Yes that's right, you thought it was the stuff of dreams,
-                      but even bananas can be bioengineered.
-                    </p>
-                  </Grid.Column>
-                  <Grid.Column floated="right" width={6}>
-                    <Image
-                      bordered
-                      rounded
-                      size="large"
-                      src="/images/wireframe/white-image.png"
-                    />
-                  </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                  <Grid.Column textAlign="center">
-                    <Button size="huge">Check Them Out</Button>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </Segment>
-
-            <Segment style={{ padding: "0em" }} vertical>
-              <Grid celled="internally" columns="equal" stackable>
-                <Grid.Row textAlign="center">
-                  <Grid.Column
-                    style={{ paddingBottom: "5em", paddingTop: "5em" }}
+                  </Transition>
+                  <Transition
+                    visible={calculations.onScreen}
+                    animation="fade up"
+                    duration={1400}
                   >
-                    <Header as="h3" style={{ fontSize: "2em" }}>
-                      "What a Company"
-                    </Header>
-                    <p style={{ fontSize: "1.33em" }}>
-                      That is what they all say about us
-                    </p>
-                  </Grid.Column>
-                  <Grid.Column
-                    style={{ paddingBottom: "5em", paddingTop: "5em" }}
-                  >
-                    <Header as="h3" style={{ fontSize: "2em" }}>
-                      "I shouldn't have gone with their competitor."
-                    </Header>
-                    <p style={{ fontSize: "1.33em" }}>
-                      <Image avatar src="/images/avatar/large/nan.jpg" />
-                      <b>Nan</b> Chief Fun Officer Acme Toys
-                    </p>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </Segment>
-
-            <Segment style={{ padding: "8em 0em" }} vertical>
-              <Container text>
-                <Header as="h3" style={{ fontSize: "2em" }}>
-                  Breaking The Grid, Grabs Your Attention
-                </Header>
-                <p style={{ fontSize: "1.33em" }}>
-                  Instead of focusing on content creation and hard work, we have
-                  learned how to master the art of doing nothing by providing
-                  massive amounts of whitespace and generic content that can
-                  seem massive, monolithic and worth your attention.
-                </p>
-                <Button as="a" size="large">
-                  Read More
-                </Button>
-
-                <Divider
-                  as="h4"
-                  className="header"
-                  horizontal
-                  style={{ margin: "3em 0em", textTransform: "uppercase" }}
-                >
-                  {/* <a href="#">Case Studies</a> */}
-                </Divider>
-
-                <Header as="h3" style={{ fontSize: "2em" }}>
-                  Did We Tell You About Our Bananas?
-                </Header>
-                <p style={{ fontSize: "1.33em" }}>
-                  Yes I know you probably disregarded the earlier boasts as
-                  non-sequitur filler content, but it's really true. It took
-                  years of gene splicing and combinatory DNA research, but our
-                  bananas can really dance.
-                </p>
-                <Button as="a" size="large">
-                  I'm Still Quite Interested
-                </Button>
-              </Container>
-            </Segment>
-
-            <Segment inverted vertical style={{ padding: "5em 0em" }}>
-              <Container>
-                <Grid divided inverted stackable>
-                  <Grid.Row>
-                    <Grid.Column width={3}>
-                      <Header inverted as="h4" content="About" />
-                      <List link inverted>
-                        <List.Item as="a">Sitemap</List.Item>
-                        <List.Item as="a">Contact Us</List.Item>
-                        <List.Item as="a">Religious Ceremonies</List.Item>
-                        <List.Item as="a">Gazebo Plans</List.Item>
-                      </List>
-                    </Grid.Column>
-                    <Grid.Column width={3}>
-                      <Header inverted as="h4" content="Services" />
-                      <List link inverted>
-                        <List.Item as="a">Banana Pre-Order</List.Item>
-                        <List.Item as="a">DNA FAQ</List.Item>
-                        <List.Item as="a">How To Access</List.Item>
-                        <List.Item as="a">Favorite X-Men</List.Item>
-                      </List>
-                    </Grid.Column>
-                    <Grid.Column width={7}>
-                      <Header as="h4" inverted>
-                        Footer Header
+                    <div>
+                      <Header as="h3" style={{ fontSize: "2em" }}>
+                        We Make Bananas That Can Dance
                       </Header>
-                      <p>
-                        Extra space for a call to action inside the footer that
-                        could help re-engage users.
+                      <p style={{ fontSize: "1.33em" }}>
+                        Yes that's right, you thought it was the stuff of
+                        dreams, but even bananas can be bioengineered.
                       </p>
-                    </Grid.Column>
-                  </Grid.Row>
-                </Grid>
-              </Container>
-            </Segment>
-          </Visibility>
-        </Ref>
+                    </div>
+                  </Transition>
+                </Grid.Column>
+                <Grid.Column floated="right" width={6}>
+                  <Image
+                    bordered
+                    rounded
+                    size="large"
+                    src="/images/wireframe/white-image.png"
+                  />
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column textAlign="center">
+                  <Transition
+                    visible={calculations.onScreen}
+                    animation="fade up"
+                    duration={1400}
+                  >
+                    <center>
+                      <Button size="huge">Check Them Out</Button>
+                    </center>
+                  </Transition>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Segment>
+          <Segment inverted vertical style={{ padding: "5em 0em" }}>
+            <Container>
+              <Grid divided inverted stackable>
+                <Grid.Row>
+                  <Grid.Column width={3}>
+                    <Header inverted as="h4" content="About" />
+                    <List link inverted>
+                      <List.Item as="a">Sitemap</List.Item>
+                      <List.Item as="a">Contact Us</List.Item>
+                      <List.Item as="a">Religious Ceremonies</List.Item>
+                      <List.Item as="a">Gazebo Plans</List.Item>
+                    </List>
+                  </Grid.Column>
+                  <Grid.Column width={3}>
+                    <Header inverted as="h4" content="Services" />
+                    <List link inverted>
+                      <List.Item as="a">Banana Pre-Order</List.Item>
+                      <List.Item as="a">DNA FAQ</List.Item>
+                      <List.Item as="a">How To Access</List.Item>
+                      <List.Item as="a">Favorite X-Men</List.Item>
+                    </List>
+                  </Grid.Column>
+                  <Grid.Column width={7}>
+                    <Header as="h4" inverted>
+                      Footer Header
+                    </Header>
+                    <p>
+                      Extra space for a call to action inside the footer that
+                      could help re-engage users.
+                    </p>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Container>
+          </Segment>
+        </Visibility>
       </ResponsiveContainer>
     );
   }
